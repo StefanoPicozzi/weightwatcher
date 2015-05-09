5f3e30559c58032811ca7e86bc04d908bfcb3185
Stefano Picozzi <StefanoPicozzi@gmail.com>
2015-05-09 10:49:04 +1000

    x

---
+++ container/heartbeat.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CONTAINER=watch
+
+while [ 1 ]
+do
+
+  for IP in 192.168.99.100
+  do
+  for PORT in 8080
+  do
+
+    isKIEContainerStarted=$(curl --connect-timeout 3 -sb -H "Accept: application/xml" -X GET -u erics:jbossbrms1! "http://$IP:$PORT/kie-server/services/rest/server/containers/$CONTAINER" | grep -c "SUCCESS")
+    echo "Started status of kie-server $IP:$PORT $CONTAINER container is $isKIEContainerStarted"
+
+    if [ $isKIEContainerStarted != "1" ]; then    
+      echo "Starting kie-server $IP:$PORT $CONTAINER container ..."
+      doKIEContainerStart=$(curl --connect-timeout 3 -sb -H "Accept: application/xml" -H "Content-Type: application/xml" -d @start.xml -X PUT -u erics:jbossbrms1! "http://$IP:$PORT/kie-server/services/rest/server/containers/$CONTAINER")
+    fi
+
+  done
+  done
+
+  sleep 10
+done
+
+
+
